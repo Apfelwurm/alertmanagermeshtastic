c4bcd4f3a113e5060260585f80f24bca0a7c2157
Alexander Volz <Alexander@volzit.de>
2023-07-29 03:03:57 +0200

    finishing

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
-# Alertmanager webhook for meshtastic (Python Version)
+# Alertmanager webhook for meshtastic
+
+This little Adapter receives alertmanager webhooks and sends the notifications via a over serial attached Meshtastic device to the specified nodeID.
 
 [![linux/amd64](https://github.com/Apfelwurm/alertmanager-webhook-meshtastic-python/actions/workflows/build-linux-image.yml/badge.svg)](https://github.com/Apfelwurm/alertmanager-webhook-meshtastic-python/actions/workflows/build-linux-image.yml)
 
-based on the work of https://github.com/nopp/alertmanager-webhook-telegram-python
-Python version 3
+This container is based on the work of https://github.com/nopp/alertmanager-webhook-telegram-python
+Thanks to [GUVWAF](https://github.com/GUVWAF) for the support and thanks to the whole meshtastic team for this awsome software!
 
-## INSTALL
-
-* pip install -r requirements.txt
-
-Alertmanager configuration example
-==================================
-
+##  Alertmanager configuration example
 	receivers:
 	- name: 'meshtastic-webhook'
 	  webhook_configs:
@@ -22,19 +18,37 @@ Alertmanager configuration example
 		username: 'XXXUSERNAME'
 		password: 'XXXPASSWORD'
 
-Running
-=======
-* python flaskAlert.py
 
-Running on docker
-=================
+##  Running on docker - Hardware Serial (default)
+
+```
     docker run -d --name alertmanager-webhook-meshtastic-python \
 		--device=/dev/ttyACM0 \
+		-e "meshtty=/dev/ttyACM0" \
     	-e "nodeID=123456789" \
     	-e "auth=true" \
     	-e "username=XXXUSERNAME" \
     	-e "password=XXXPASSWORD" \
+    	-e "loglevel=WARNING" \
     	-p 9119:9119 apfelwurm/alertmanager-webhook-meshtastic-python:latest
+```
+
+##  Running on docker - Virtual Serial
+
+If you plan to use a virtual serial port that is provided with socat (for example /tmp/vcom0), you have to use a volume mount instead of the device binding:
+
+```
+    docker run -d --name alertmanager-webhook-meshtastic-python \
+		-v /tmp/vcom0:/tmp/vcom0 \
+		-e "meshtty=/tmp/vcom0" \
+    	-e "nodeID=123456789" \
+    	-e "auth=true" \
+    	-e "username=XXXUSERNAME" \
+    	-e "password=XXXPASSWORD" \
+    	-e "loglevel=WARNING" \
+    	-p 9119:9119 apfelwurm/alertmanager-webhook-meshtastic-python:latest
+```
+
 
 Example to test
 ===============
--- python/flaskAlert.py
+++ python/flaskAlert.py
@@ -4,17 +4,13 @@ from dateutil import parser
 from flask import Flask
 from flask import request
 from flask_basicauth import BasicAuth
-from pubsub import pub
-import threading
 
 app = Flask(__name__)
 app.secret_key = 'changeKeyHeere'
 basic_auth = BasicAuth(app)
 
-# # Yes need to have -, change it!
 nodeID = int(os.getenv('nodeID'))
 
-# Authentication conf, change it!
 app.config['BASIC_AUTH_FORCE'] = os.getenv('auth')
 app.config['BASIC_AUTH_USERNAME'] = os.getenv('username')
 app.config['BASIC_AUTH_PASSWORD'] = os.getenv('password')
@@ -22,26 +18,19 @@ app.config['BASIC_AUTH_PASSWORD'] = os.getenv('password')
 interface =  meshtastic.serial_interface.SerialInterface(os.getenv('meshtty'))
 
 
-
-# def onreceive(packet, interface):  # pylint: disable=unused-argument
-#     """called when a packet arrives"""
-#     app.logger.debug("\treceived: %s",packet)
-#     try:
-#         # interface.getNode(nodeID, False).iface.waitForAckNak()
-#         interface.waitForAckNak()
-#         app.logger.debug("\t ack: %s",packet)
-#     except Exception as error:
-#         app.logger.error("\trecverror: %s",error)
-
-
-# def meshthread():
-#     pub.subscribe(onreceive, "meshtastic.receive")
-#     app.logger.debug("\thallofromthread")
-
 with app.app_context():
-    app.logger.setLevel(logging.DEBUG)
-    # threading.Thread(target=meshthread, daemon=True).start()
-    # app.logger.debug("\thallo")
+    if os.getenv('loglevel') == "DEBUG":
+        app.logger.setLevel(logging.DEBUG)
+    if os.getenv('loglevel') == "WARNING":
+        app.logger.setLevel(logging.WARNING)
+    if os.getenv('loglevel') == "CRITICAL":
+        app.logger.setLevel(logging.CRITICAL)
+    if os.getenv('loglevel') == "ERROR":
+        app.logger.setLevel(logging.ERROR)
+    if os.getenv('loglevel') == "INFO":
+        app.logger.setLevel(logging.INFO)
+
+
 
 @app.route('/alert', methods = ['POST'])
 def postalertmanager():
@@ -60,16 +49,14 @@ def postalertmanager():
                 message += "Info: "+alert['annotations']['info']+"\n"
             if 'summary' in alert['annotations']:
                 message += "Summary: "+alert['annotations']['summary']+"\n"                
-            # if 'description' in alert['annotations']:
-            #     message += "Description: "+alert['annotations']['description']+"\n"
+            if 'description' in alert['annotations']:
+                message += "Description: "+alert['annotations']['description']+"\n"
             if alert['status'] == "resolved":
                 correctDate = parser.parse(alert['endsAt']).strftime('%Y-%m-%d %H:%M:%S')
                 message += "Resolved: "+correctDate
             elif alert['status'] == "firing":
                 correctDate = parser.parse(alert['startsAt']).strftime('%Y-%m-%d %H:%M:%S')
                 message += "Started: "+correctDate
-            # app.logger.info("\t%s",message)
-            # interface.sendText(message, nodeID)
             app.logger.debug("\tMessage:%s", message)
             interface.sendText(message, nodeID, True,False,interface.getNode(nodeID, False).onAckNak)
             interface.waitForAckNak()
