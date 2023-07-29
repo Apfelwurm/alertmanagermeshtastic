6fa9b96e910b2e69807cfe9853f9ebad586f126d
Alexander Volz <Alexander@volzit.de>
2023-07-29 19:34:09 +0200

    test

--- python/flaskAlert.py
+++ python/flaskAlert.py
@@ -20,31 +20,86 @@ app.config["BASIC_AUTH_PASSWORD"] = os.getenv("password")
 # Create a single-threaded queue
 data_queue = queue.Queue()
 
+
 # Worker function that processes the queue
 def process_queue():
     while True:
         try:
-            alert = data_queue.get()          
-            interface = meshtastic.serial_interface.SerialInterface(os.getenv("meshtty"))
-            app.logger.debug("\t %s", alert["fingerprint"])
-            interface.sendText(
-                            alert["fingerprint"],
+            alert = data_queue.get()
+            try:
+                app.logger.debug("\t queue working on %s", alert["fingerprint"])
+
+                app.logger.debug(
+                    "\t========================================================================================================"
+                )
+                app.logger.debug("\tprocessing alert")
+                app.logger.debug("\t %s", alert)
+                app.logger.debug(
+                    "\t========================================================================================================"
+                )
+                message = formatalert(alert)
+                app.logger.debug(
+                    "\t========================================================================================================"
+                )
+                app.logger.debug("\tFull message:")
+                app.logger.debug("\t%s", message)
+                app.logger.debug(
+                    "\t========================================================================================================"
+                )
+
+            except Exception as e:
+                app.logger.error(
+                    "\t Message failed: %s",
+                    e,
+                )
+                raise
+
+            try:
+                chunks = splitmessagesifnessecary(message)
+
+            except Exception as e:
+                app.logger.error(
+                    "\t could not split in chunks: %s",
+                    e,
+                )
+                raise
+
+            for chunk in chunks:
+                for attempt in range(max_sending_attempts):
+                    app.logger.debug("\sending chunk attempt %d :%s ", attempt, chunk)
+                    try:
+                        interface = meshtastic.serial_interface.SerialInterface(
+                            os.getenv("meshtty")
+                        )
+                        interface.sendText(
+                            chunk,
                             nodeID,
                             True,
                             False,
                             interface.getNode(nodeID, False).onAckNak,
                         )
-            interface.waitForAckNak()
-            interface.close()
+                        interface.waitForAckNak()
+                        app.logger.debug("\sending chunk attempt %d success ", attempt)
+                        break
+                    except Exception as e:
+                        app.logger.error(
+                            "\t chunk Attempt %d failed with error: %s",
+                            attempt + 1,
+                            e,
+                        )
+                        if attempt == max_sending_attempts - 1:
+                            raise
+                    finally:
+                        interface.close()
             data_queue.task_done()
         except queue.Empty:
             break
+
 
 # Thread to run the worker function
 worker_thread = threading.Thread(target=process_queue)
 worker_thread.daemon = True
 worker_thread.start()
-
 
 
 with app.app_context():
@@ -60,16 +115,49 @@ with app.app_context():
         app.logger.setLevel(logging.INFO)
 
 
-# def splitmessagesifnessecary(message):
+def splitmessagesifnessecary(message):
+    chunk_size = 199
+    if len(message) > chunk_size:
+        app.logger.debug("\tMessage to big, split to chunks")
+        chunks = [
+            message[i : i + chunk_size] for i in range(0, len(message), chunk_size)
+        ]
+        return chunks
+    else:
+        app.logger.debug("\tMessage size okay")
+        return [message]
     
-#     chunk_size = 199
-#     if len(message) > chunk_size:
-#         app.logger.debug("\tMessage to big, split to chunks")
-#         chunks = [message[i : i + chunk_size] for i in range(0, len(message), chunk_size)]
-#         return chunks
-#     else:
-#         app.logger.debug("\tMessage size okay")
-#         return [message]
+
+def formatalert(alert):
+    message = "Status: " + alert["status"] + "\n"
+    if "name" in alert["labels"]:
+        message += (
+            "Instance: "
+            + alert["labels"]["instance"]
+            + "("
+            + alert["labels"]["name"]
+            + ")\n"
+        )
+    elif "instance" in alert["labels"]:
+        message += "Instance: " + alert["labels"]["instance"] + "\n"
+    elif "alertname" in alert["labels"]:
+        message += "Alert: " + alert["labels"]["alertname"] + "\n"
+    if "info" in alert["annotations"]:
+        message += "Info: " + alert["annotations"]["info"] + "\n"
+    if "summary" in alert["annotations"]:
+        message += "Summary: " + alert["annotations"]["summary"] + "\n"
+    if alert["status"] == "resolved":
+        correctdate = parser.parse(alert["endsAt"]).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        message += "Resolved: " + correctdate
+    elif alert["status"] == "firing":
+        correctdate = parser.parse(alert["startsAt"]).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        message += "Started: " + correctdate
+    return message
+
 
 
 @app.route("/alert", methods=["POST"])
@@ -78,7 +166,7 @@ def postalertmanager():
         content = json.loads(request.get_data())
         for alert in content["alerts"]:
             data_queue.put(alert)
-        
+
         return "Alert OK", 200
     except Exception as error:
         app.logger.error(
