01cf517fb3283965233178a9b0ab53a32cf83114
Alexander Volz <Alexander@volzit.de>
2023-07-29 19:09:00 +0200

    test queue

--- python/flaskAlert.py
+++ python/flaskAlert.py
@@ -4,6 +4,8 @@ from dateutil import parser
 from flask import Flask
 from flask import request
 from flask_basicauth import BasicAuth
+import queue
+import threading
 
 app = Flask(__name__)
 app.secret_key = "changeKeyHeere"
@@ -15,7 +17,26 @@ app.config["BASIC_AUTH_FORCE"] = os.getenv("auth")
 app.config["BASIC_AUTH_USERNAME"] = os.getenv("username")
 app.config["BASIC_AUTH_PASSWORD"] = os.getenv("password")
 
+# Create a single-threaded queue
+data_queue = queue.Queue()
+
 interface = meshtastic.serial_interface.SerialInterface(os.getenv("meshtty"))
+
+# Worker function that processes the queue
+def process_queue():
+    while True:
+        try:
+            alert = data_queue.get()          
+            app.logger.debug("\t %s", alert["fingerprint"])
+            data_queue.task_done()
+        except queue.Empty:
+            break
+
+# Thread to run the worker function
+worker_thread = threading.Thread(target=process_queue)
+worker_thread.daemon = True
+worker_thread.start()
+
 
 
 with app.app_context():
@@ -31,101 +52,29 @@ with app.app_context():
         app.logger.setLevel(logging.INFO)
 
 
-def splitmessagesifnessecary(message):
+# def splitmessagesifnessecary(message):
     
-    chunk_size = 199
-    if len(message) > chunk_size:
-        app.logger.debug("\tMessage to big, split to chunks")
-        chunks = [message[i : i + chunk_size] for i in range(0, len(message), chunk_size)]
-        return chunks
-    else:
-        app.logger.debug("\tMessage size okay")
-        return [message]
+#     chunk_size = 199
+#     if len(message) > chunk_size:
+#         app.logger.debug("\tMessage to big, split to chunks")
+#         chunks = [message[i : i + chunk_size] for i in range(0, len(message), chunk_size)]
+#         return chunks
+#     else:
+#         app.logger.debug("\tMessage size okay")
+#         return [message]
 
 
 @app.route("/alert", methods=["POST"])
 def postalertmanager():
     try:
-        # define the serial interface
-        # get content
         content = json.loads(request.get_data())
-        app.logger.debug("\t========================================================================================================")
-        app.logger.debug("\tpost received")
-        app.logger.debug("\t%s", content)
-        app.logger.debug("\t========================================================================================================")
-
         for alert in content["alerts"]:
-            app.logger.debug("\t========================================================================================================")
-            app.logger.debug("\tprocessing alert")
-            app.logger.debug("\t %s", alert)
-            app.logger.debug("\t========================================================================================================")
-            message = "Status: " + alert["status"] + "\n"
-            if "name" in alert["labels"]:
-                message += (
-                    "Instance: "
-                    + alert["labels"]["instance"]
-                    + "("
-                    + alert["labels"]["name"]
-                    + ")\n"
-                )
-            elif "instance" in alert["labels"]:
-                message += "Instance: " + alert["labels"]["instance"] + "\n"
-            elif "alertname" in alert["labels"]:
-                message += "Alert: " + alert["labels"]["alertname"] + "\n"
-            if "info" in alert["annotations"]:
-                message += "Info: " + alert["annotations"]["info"] + "\n"
-            if "summary" in alert["annotations"]:
-                message += "Summary: " + alert["annotations"]["summary"] + "\n"
-            # if "description" in alert["annotations"]:
-            #     message += (
-            #         "Description: " + alert["annotations"]["description"] + "\n"
-            #     )
-            if alert["status"] == "resolved":
-                correctdate = parser.parse(alert["endsAt"]).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                )
-                message += "Resolved: " + correctdate
-            elif alert["status"] == "firing":
-                correctdate = parser.parse(alert["startsAt"]).strftime(
-                    "%Y-%m-%d %H:%M:%S"
-                )
-                message += "Started: " + correctdate
-            app.logger.debug("\t========================================================================================================")
-            app.logger.debug("\tFull message:")
-            app.logger.debug("\t%s", message)
-            app.logger.debug("\t========================================================================================================")
-
-
-            chunks = splitmessagesifnessecary(message)
-            for chunk in chunks:
-                for attempt in range(max_sending_attempts):
-                    app.logger.debug("\sending chunk attempt %d :%s ", attempt, chunk)
-                    try:
-                        interface.sendText(
-                            chunk,
-                            nodeID,
-                            True,
-                            False,
-                            interface.getNode(nodeID, False).onAckNak,
-                        )
-                        interface.waitForAckNak()
-                        app.logger.debug("\sending chunk attempt %d success ", attempt)
-                        break
-                    except Exception as e:
-                        app.logger.error(
-                            "\t chunk Attempt %d failed with error: %s",
-                            attempt + 1,
-                            e,
-                        )
-                        if (
-                            attempt == max_sending_attempts - 1
-                        ):
-                            raise 
+            data_queue.put(alert)
+        
         return "Alert OK", 200
     except Exception as error:
         app.logger.error(
-            "\tAt least one alert could not be delivered after %d attempts, the error is: %s ",
-            max_sending_attempts,
+            "\t could not queue alerts: %s ",
             error,
         )
         return "Alert fail", 200
