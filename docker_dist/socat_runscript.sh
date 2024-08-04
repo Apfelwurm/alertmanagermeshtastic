d638b19f7ff74600096cb481e82eb2482dacd015
Apfelwurm <Alexander@volzit.de>
2024-08-04 14:50:33 +0200

    test socat timeout

--- docker_dist/socat_runscript.sh
+++ docker_dist/socat_runscript.sh
@@ -3,6 +3,6 @@ set +e
 
 while true
 do
-   socat pty,link=/tmp/vcom0,raw,group-late=dialout,mode=660 $SOCAT_CONNECTION
+   socat -t5 pty,link=/tmp/vcom0,raw,group-late=dialout,mode=660 $SOCAT_CONNECTION
    sleep 2
 done
